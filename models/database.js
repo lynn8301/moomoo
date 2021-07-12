f573cd081053b26a338b3b68faa0aeedfb0a4be7
lynn <lynn@blueplanet.com>
2021-07-12 14:21:37 +0800

    modify

--- models/database.js
+++ models/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize')
 const base = require('../lib/base')
 const sequelize = new Sequelize(
-    base.config().database,
-    base.config().username,
-    base.config().password,
+    base.config().mysql.database,
+    base.config().mysql.username,
+    base.config().mysql.password,
     {
-        host: base.config().host,
-        dialect: base.config().dialect,
+        host: base.config().mysql.host,
+        dialect: base.config().mysql.dialect,
     }
 )
 
